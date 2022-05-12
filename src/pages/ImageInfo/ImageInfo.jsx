16f5b9dd1889509fdb4fb1f369c4ed7e2d6ad333
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2022-05-12 19:26:31 +0300

    dynamic route made

--- src/pages/ImageInfo/ImageInfo.jsx
+++ src/pages/ImageInfo/ImageInfo.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { sagaActions } from "../../redux/sagaActions";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { sagaActions } from '../../redux/sagaActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 function ImageInfo() {
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -11,15 +12,58 @@ function ImageInfo() {
       type: sagaActions.FETCH_IMAGE_SAGA,
       args: id,
     });
-  }, []);
+  }, [id]);
 
-  //{imageId}
-  // const image = useSelector((state) => state);
-  // console.log(image);
+  const image = useSelector((state) => state.images.image);
   return (
     <div>
-      id картинки {id}
-      {/* <img src={image.urls.regular} /> */}
+      <Link to={`/`}>Back to main</Link>
+      <div>
+        {Object.keys(image).length > 0 ? (
+          <>
+            <img
+              src={image.urls.regular}
+              alt={image.description ? image.description : null}
+            />
+            <div>
+              <p>{image.description ? image.description : null}</p>
+              <ul>{image.description ? image.description : null}</ul>
+              <p>
+                Likes:
+                {image.tags_preview.length > 0
+                  ? image.tags_preview.map((tag) => {
+                      return <li key={`${tag}_${tag.title}`}>{tag.title}</li>;
+                    })
+                  : null}
+              </p>
+              <p>
+                Link on unsplash:{' '}
+                {image.links.html ? (
+                  <a href={image.links.html} target="_blank" rel="noreferrer">
+                    link
+                  </a>
+                ) : null}
+              </p>
+
+              <div>
+                Related images{' '}
+                {image.related_collections.results.length > 0
+                  ? image.related_collections.results.map((img) => {
+                      return (
+                        <img
+                          src={img.cover_photo.urls.small}
+                          alt={img.title ? img.title : 'title'}
+                        />
+                      );
+                    })
+                  : 'no related collections'}
+              </div>
+            </div>
+          </>
+        ) : (
+          <p>Loading</p>
+        )}
+      </div>
     </div>
   );
 }
